938f27d6c990e78be75bb8f7dc8fd811620ffe98
Yeahyun Kim <insidesy4@gmail.com>
2024-03-12 17:28:56 +0900

    [Study] Java 입문 - 연산자 문제 풀기

---
+++ java-start/src/operator/ex/OperationEx3.java
@@ -0,0 +1,11 @@
+package Operator.ex;
+
+public class OperationEx3 {
+    public static void main(String[] args) {
+        int score = 80;
+
+        boolean result = 100 >= score && score >= 80;
+        System.out.println(result);
+
+    }
+}
