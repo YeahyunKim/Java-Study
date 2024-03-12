4cd7e4ceea79a390174d99f3ce23b83898e9466a
Yeahyun Kim <insidesy4@gmail.com>
2024-03-12 14:37:31 +0900

    [Study] Java 입문 - 전위, 후위 증감 연산

---
+++ java-start/src/operator/OperatorAdd1.java
@@ -0,0 +1,13 @@
+package Operator;
+
+public class OperatorAdd1 {
+    public static void main(String[] args) {
+        int a = 0;
+
+        ++a; //1
+        System.out.println(a);
+
+        ++a; //2
+        System.out.println(a);
+    }
+}
