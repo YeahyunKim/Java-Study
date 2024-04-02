e593049014bd5815f28e42767dc1d2321a2a8b7c
Yeahyun Kim <insidesy4@gmail.com>
2024-04-02 14:39:06 +0900

    [Add] Java-Basic 기본편 파일 추가

--- java-start/src/method/Overloading2.java
+++ java-start/src/method/Overloading2.java
@@ -4,7 +4,21 @@ public class Overloading2 {
     public static void main(String[] args) {
         myMethod(1, 1.2);
         myMethod(1.2, 1);
+
+        String[] names2 = {"1", "2", "3"};
+        for(int i = 0; i <= names2.length; i++) {
+            System.out.println(i);
+        }
+
+        String test = "03";
+        System.out.println(Integer.valueOf(test) > 2);
+
+//        test.replace("l", "");
+
+        System.out.println(test);
+
     }
+
 
     public static void myMethod(int a, double b) {
         System.out.println("int a, double b");
