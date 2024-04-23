414324ce90cd796fb10082420519f21cdebfbced
Yeahyun Kim <insidesy4@gmail.com>
2024-04-23 14:07:51 +0900

    [Study] Java 기초 - 캡슐화 2차

--- java-basic/src/access/b/BankAccountMain.java
+++ java-basic/src/access/b/BankAccountMain.java
@@ -2,9 +2,11 @@ package access.b;
 
 public class BankAccountMain {
     public static void main(String[] args) {
-        BankAccount account = new BankAccount();
-        account.deposit(10000);
-        account.deposit(10000);
-        System.out.println(account.getBalance());
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(10000);
+        bankAccount.withdraw(12000);
+
+        System.out.println("남은 잔액은 " + bankAccount.getBalance() + "원 입니다.");
     }
 }
+
